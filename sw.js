89314faec5dc34538e00830edea1fc76828bdb45
Abhay <badhwarabhay@gmail.com>
2026-08-16 02:35:15 -0400

    Refresh app cache for EXIF analyzer

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='canon-t7-studio-v5';
+const CACHE='canon-t7-studio-v6';
 const CORE=['./','./index.html','./styles.css','./app.js','./conditions.js','./editor.js','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
